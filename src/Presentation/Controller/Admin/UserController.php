2b3cc6cd2cf58f7c0a1b2924c775eed375514f81
magicbart <magicbart@gmail.com>
2026-02-22 10:50:40 +0100

    Add admin users

---
+++ src/Presentation/Controller/Admin/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Controller\Admin;
+
+use App\Domain\Entity\User;
+use App\Infrastructure\Doctrine\Repository\UserRepository;
+use App\Presentation\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin/users')]
+#[IsGranted('ROLE_SUPER_ADMIN')]
+class UserController extends AbstractController
+{
+    #[Route('', name: 'admin_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('@App/admin/users/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'admin_user_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+    ): Response {
+        $user = new User();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+            $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'User created successfully.');
+
+            return $this->redirectToRoute('admin_user_index');
+        }
+
+        return $this->render('@App/admin/users/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_user_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+    ): Response {
+        $form = $this->createForm(UserFormType::class, $user, ['is_edit' => true]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string|null $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            if ($plainPassword !== null && $plainPassword !== '') {
+                $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'User updated successfully.');
+
+            return $this->redirectToRoute('admin_user_index');
+        }
+
+        return $this->render('@App/admin/users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'admin_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $token = $request->request->getString('_token');
+
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $token)) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'User deleted successfully.');
+        }
+
+        return $this->redirectToRoute('admin_user_index');
+    }
+}
